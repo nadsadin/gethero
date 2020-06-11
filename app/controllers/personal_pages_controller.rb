b2937e7126b7d635c0fccf8483ed86952623dd47
Nadsadin <leloushlampergh@gmail.com>
2020-06-11 23:55:01 +0300

    update personal page

--- app/controllers/personal_pages_controller.rb
+++ app/controllers/personal_pages_controller.rb
@@ -2,6 +2,7 @@ class PersonalPagesController < ApplicationController
 
   def show
     @personal_page = PersonalPage.friendly.find(params[:id])
+    @back_default = root_path
     render layout: 'clientside/standard'
   end
 end
